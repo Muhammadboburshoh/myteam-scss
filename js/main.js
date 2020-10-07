1eeed98256bded25e68d002c69781f5768da3ecb
Muhammadboburshoh <muhammadbobur600@gmail.com>
2020-10-07 19:19:44 +0500

    finish header

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".navmenu-togler")
+const navmenu = document.querySelector(".sitenav__list")
+
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("navmenu-togler")
+  navmenu.classList.toggle("open-menu")
+  // hamburger.classList.toggle("open-button")
+
+})
+
