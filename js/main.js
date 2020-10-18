8d65fbd88b8dbc64542a36a60fbe701e09a0d684
Muhammadboburshoh <muhammadbobur600@gmail.com>
2020-10-18 12:31:04 +0500

    Add director open in close about section

--- js/main.js
+++ js/main.js
@@ -3,7 +3,21 @@ const navmenu = document.querySelector(".sitenav__list")
 
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("navmenu-togler")
   navmenu.classList.toggle("open-menu")
   hamburger.classList.toggle("open-button")
+})
+
+const directorButton = document.querySelector(".director__about-open")
+const directorEbout = document.querySelector(".actev-wrapper")
+const directorOpen = document.querySelector(".direction__about-wrapper")
+
+
+directorButton.addEventListener("click", () => {
+  directorButton.classList.toggle("direction__about-wrapper-open")
+
+  directorEbout.classList.toggle("actev-wrapper")
+  directorEbout.classList.toggle("direction__about-wrapper")
+
+  directorOpen.classList.toggle("direction__about-wrapper")
+  directorOpen.classList.toggle("actev-wrapper")
 })
