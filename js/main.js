8a43a77a01e2188821f0a556b459710fb85e5ed5
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-03-27 01:26:48 +0500

    Edit header responsev menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const hamburger = document.querySelector(".navmenu-togler")
 const navmenu = document.querySelector(".sitenav__list")
 
+console.log(hamburger);
 
 hamburger.addEventListener("click", () => {
   navmenu.classList.toggle("open-menu")
   hamburger.classList.toggle("open-button")
+  // hamburger.classList.toggle("navmenu-togler")
 })
 
 
@@ -18,14 +20,15 @@ for (let i = 0; i < directorButtons.length; i++) {
 
   directorButtons[i].addEventListener("click", () => {
 
+    directorButtons[i].classList.toggle("direction__about-wrapper-open")
+    directorButtons[i].classList.toggle("director__about-open")
+
     directorEbouts[i].classList.toggle("director__active")
     directorEbouts[i].classList.toggle("directors__close")
   
     directorOpens[i].classList.toggle("directors__close")
     directorOpens[i].classList.toggle("director__active")
 
-    directorButtons[i].classList.toggle("director__about-open")
-    directorButtons[i].classList.toggle("direction__about-wrapper-open")
   })
 }
 
